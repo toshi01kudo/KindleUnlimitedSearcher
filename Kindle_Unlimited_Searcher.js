99151e6dbac4b978087fae4c857e308590493d29
toshi01kudo <72183025+toshi01kudo@users.noreply.github.com>
2020-12-03 09:05:01 +0900

    Add button to get strings for #1, but doesnt work.

--- Kindle_Unlimited_Searcher.js
+++ Kindle_Unlimited_Searcher.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kindle Unlimited Searcher
 // @namespace    Kindle Unlimited Searcher
-// @version      0.1
+// @version      0.2
 // @description  Create a link to search any kindle book selling with Kindle Unlimited.
 // @author       Toshihito Kudo
 // @include      https://www.amazon.co.jp*
@@ -11,14 +11,22 @@
 (function() {
     'use strict';
 
-    const preUrlStr = "https://www.amazon.co.jp/s?k=";
-    const SchStr = $("#twotabsearchtextbox").value;
-    const SchStr2 = $(".a-color-state.a-text-bold").textContent;
-    const postUrlStr = "&i=digital-text&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&dc&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&qid=1606401522&rnid=3169285051&ref=sr_nr_p_n_feature_nineteen_browse-bin_1";
-    console.log(SchStr);
-    console.log(SchStr2);
+    //const SchStr = $("#twotabsearchtextbox").value;
+    //const SchStr2 = $(".a-color-state.a-text-bold").textContent;
+    //console.log(SchStr);
+    //console.log(SchStr2);
 
-    const SchUrl = preUrlStr + SchStr + postUrlStr;
-    console.log(SchUrl);
-    $(".nav-right").prepend(`<a herf="${SchUrl}">KU_Search</a>`);
+    // クリックすると検索窓に打ち込んだ内容を読み込んで、Kindle Unlimited 対象のもののみ検索する
+    $(".nav-right").prepend(`<button id="KU_Sch_Btn" type="button">KU_Search</button>`);
+    $("#KU_Sch_Btn").click(function(){
+        const preUrlStr = "https://www.amazon.co.jp/s?k=";
+        const postUrlStr = "&i=digital-text&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&dc&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&qid=1606401522&rnid=3169285051&ref=sr_nr_p_n_feature_nineteen_browse-bin_1";
+        const SchStr = $("#twotabsearchtextbox").value;
+        console.log(SchStr);
+        const SchUrl = preUrlStr + SchStr + postUrlStr;
+        console.log(SchUrl);
+    })
+
+    //console.log(SchUrl);
+    //$(".nav-right").prepend(`<a herf="${SchUrl}">KU_Search</a>`);
 })();
