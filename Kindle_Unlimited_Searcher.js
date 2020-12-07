6d090b2a0fff8c47ca93ba21702a52d205a559a7
toshi01kudo <72183025+toshi01kudo@users.noreply.github.com>
2020-12-07 09:24:29 +0900

    Include ID selector to create button for #2

--- Kindle_Unlimited_Searcher.js
+++ Kindle_Unlimited_Searcher.js
@@ -17,7 +17,7 @@
     //console.log(SchStr2);
 
     // クリックすると検索窓に打ち込んだ内容を読み込んで、Kindle Unlimited 対象のもののみ検索する
-    $(".nav-right").prepend(`<button id="KU_Sch_Btn" type="button">KU_Search</button>`);
+    $("#nav-search .nav-right").prepend(`<button id="KU_Sch_Btn" type="button">KU_Search</button>`);
     $("#KU_Sch_Btn").click(function(){
         const preUrlStr = "https://www.amazon.co.jp/s?k=";
         const postUrlStr = "&i=digital-text&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&dc&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&qid=1606401522&rnid=3169285051&ref=sr_nr_p_n_feature_nineteen_browse-bin_1";
