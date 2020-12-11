5fe352f6edc675e4fcad98b27403a5b3195fe027
toshi01kudo <72183025+toshi01kudo@users.noreply.github.com>
2020-12-11 21:03:50 +0900

    Change color and size of the button for #3

--- Kindle_Unlimited_Searcher.user.js
+++ Kindle_Unlimited_Searcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kindle Unlimited Searcher
 // @namespace    Kindle Unlimited Searcher
-// @version      1.3
+// @version      1.4
 // @description  Create a link to search any kindle book selling with Kindle Unlimited.
 // @author       Toshihito Kudo
 // @include      https://www.amazon.co.jp*
@@ -14,6 +14,7 @@
     // ID nav-search配下のnav-rightクラスの前（検索ボタンの前）にボタンを生成。
     $("#nav-search .nav-right").prepend(`<button id="KU_Sch_Btn" type="button">KU_Sch</button>`);
     // クリックすると検索窓に打ち込んだ内容を読み込んで、Kindle Unlimited 対象のもののみ検索する
+    $("#KU_Sch_Btn").css({"background-color":"orange","font-size":"80%","color":"black"});
     $("#KU_Sch_Btn").click(function(){
         const preUrlStr = "https://www.amazon.co.jp/s?k=";
         const postUrlStr = "&i=digital-text&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051&dc&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&qid=1606401522&rnid=3169285051&ref=sr_nr_p_n_feature_nineteen_browse-bin_1";
@@ -26,19 +27,26 @@
         window.location.href = SchUrl;
     });
 
-    // 各種リンクを生成
+    //// 各種リンク先へ飛ぶボタンを生成
     // https://www.amazon.co.jp/gp/kindle/ku/ku_central?ref_=nav_AccountFlyout_ku
     // https://www.amazon.co.jp/hz/mycd/myx?ref_=nav_AccountFlyout_myk
     // https://read.amazon.co.jp/kp/notebook
-    $("#nav-tools").prepend(`<button id="KU_Central" type="button">KU_Ctrl</button>`);
+    $("#nav-tools").prepend(`<button id="KU_Central" type="button">KU<br>Central</button>`);
+    $("#KU_Central").after(`<button id="Kdl_MyBooks" type="button">My<br>Books</button>`);
+    $("#Kdl_MyBooks").after(`<button id="Kdl_HighLights" type="button">DogEar</button>`);
+
+    // ボタンのスタイルを指定
+    $("#KU_Central").css({"background-color":"navy","font-size":"80%","color":"white"});
+    $("#Kdl_MyBooks").css({"background-color":"navy","font-size":"80%","color":"white"});
+    $("#Kdl_HighLights").css({"background-color":"white","font-size":"100%","color":"turquoise","font-weight":"bold"});
+
+    // ボタンのリンク先を指定
     $("#KU_Central").click(function(){
         window.location.href = 'https://www.amazon.co.jp/gp/kindle/ku/ku_central?ref_=nav_AccountFlyout_ku';
     });
-    $("#KU_Central").after(`<button id="Kdl_MyBooks" type="button">My_Books</button>`);
     $("#Kdl_MyBooks").click(function(){
         window.location.href = 'https://www.amazon.co.jp/hz/mycd/myx?ref_=nav_AccountFlyout_myk';
     });
-    $("#Kdl_MyBooks").after(`<button id="Kdl_HighLights" type="button">My_DogEar</button>`);
     $("#Kdl_HighLights").click(function(){
         window.location.href = 'https://read.amazon.co.jp/kp/notebook';
     });
